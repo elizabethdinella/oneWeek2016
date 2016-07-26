c3174ac2d2ba46a9d897116fee22323f6615eff8
elizabethdinella <elizabeth.a.dinella@gmail.com>
2016-07-26 13:05:07 -0400

    added name of sender

--- appread/app.controller.js
+++ appread/app.controller.js
@@ -47,8 +47,10 @@
                   }
                   $scope.sentimentScore = results.documents[0].score;
                   console.log($scope.sentimentScore);
-                  var badMessage = "It looks like ___ isn't in a good mood, send them a dog to make them feel better!"
-                  var goodMessage = "It looks like ___  is already happy, but send them a dog to make them even happier!"
+                  var from = Office.context.mailbox.item.from;
+                  var prefix = "It looks like " + from.displayName + " ";
+                  var badMessage = prefix + "isn't in a good mood, send them a dog to make them feel better!"
+                  var goodMessage = prefix + "is already happy, but send them a dog to make them even happier!"
                   var displayMessage = "";
                   if($scope.sentimentScore >= .5){
                       displayMessage = goodMessage;
@@ -85,82 +87,6 @@
                 
                 console.log($scope.sentimentScore);
             });
-        }
-
-/*        // Get the email addresses of all the recipients of the composed item.
-        function getAllRecipients() {
-            // Local objects to point to recipients of either
-            // the appointment or message that is being composed.
-            // bccRecipients applies to only messages, not appointments.
-            var toRecipients, ccRecipients, bccRecipients;
-            // Verify if the composed item is an appointment or message.
-            if (item.itemType == Office.MailboxEnums.ItemType.Appointment) {
-                toRecipients = item.requiredAttendees;
-                ccRecipients = item.optionalAttendees;
-            }
-            else {
-                toRecipients = item.to;
-                ccRecipients = item.cc;
-                bccRecipients = item.bcc;
-            }*/
-
-            // Use asynchronous method getAsync to get each type of recipients
-            // of the composed item. Each time, this example passes an anonymous 
-            // callback function that doesn't take any parameters.
-            /*toRecipients.getAsync(function (asyncResult) {
-                if (asyncResult.status == Office.AsyncResultStatus.Failed){
-                    write(asyncResult.error.message);
-                }
-                else {
-                    // Async call to get to-recipients of the item completed.
-                    // Display the email addresses of the to-recipients. 
-                    write ('To-recipients of the item:');
-                    displayAddresses(asyncResult);
-                    $scope.getTime(asyncResult.value[0].emailAddress);
-                }    
-            }); // End getAsync for to-recipients.
-
-            // Get any cc-recipients.
-            ccRecipients.getAsync(function (asyncResult) {
-                if (asyncResult.status == Office.AsyncResultStatus.Failed){
-                    write(asyncResult.error.message);
-                }
-                else {
-                    // Async call to get cc-recipients of the item completed.
-                    // Display the email addresses of the cc-recipients.
-                    write ('Cc-recipients of the item:');
-                    displayAddresses(asyncResult);
-                }
-            }); // End getAsync for cc-recipients.
-
-            // If the item has the bcc field, i.e., item is message,
-            // get any bcc-recipients.
-            if (bccRecipients) {
-                bccRecipients.getAsync(function (asyncResult) {
-                if (asyncResult.status == Office.AsyncResultStatus.Failed){
-                    write(asyncResult.error.message);
-                }
-                else {
-                    // Async call to get bcc-recipients of the item completed.
-                    // Display the email addresses of the bcc-recipients.
-                    write ('Bcc-recipients of the item:');
-                    displayAddresses(asyncResult);
-                }
-
-                }); // End getAsync for bcc-recipients.
-             }
-        }*/
-
-        // Recipients are in an array of EmailAddressDetails
-        // objects passed in asyncResult.value.
-        function displayAddresses (asyncResult) {
-            for (var i=0; i<asyncResult.value.length; i++)
-                write (asyncResult.value[i].emailAddress);
-        }
-
-        // Writes to a div with id='message' on the page.
-        function write(message){
-            document.getElementById('message').innerText += message; 
         }
              
         
